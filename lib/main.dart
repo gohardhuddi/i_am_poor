74797c0e69b81b041eee81a28733e9f1ffc72edc
Khurram Shahzad <gohardhuddi@gmail.com>
2021-02-11 04:10:14 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("I am Poor"),
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage("images/garbage.png"),
+          ),
+        ),
+      ),
+    );
+  }
+}
